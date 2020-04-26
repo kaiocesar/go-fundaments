f47b0d77008affaa4d48c584fe006426d459e966
Kaio Cesar <tecnico.kaio@gmail.com>
2020-04-26 10:54:16 -0300

    Structure and list of products

--- main.go
+++ main.go
@@ -20,5 +20,10 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Nome: "Camiseta", Descricao: "camiseta do verão", Preco: 123.0, Quantidade: 100},
+		{Nome: "Notebook", Descricao: "acer bolado", Preco: 1000.01, Quantidade: 10},
+		{Nome: "Tenis Nike SB", Descricao: "Nike bom pra sk8", Preco: 250.05, Quantidade: 50},
+	}
+	templates.ExecuteTemplate(w, "Index", products)
 }
